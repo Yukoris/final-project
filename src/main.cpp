0c9eec162049dbda2f5ff9aea53c3f9d0b0ee386
Yukoris <103787457+Yukoris@users.noreply.github.com>
2026-05-06 13:21:09 -0400

    rewriting code

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,33 @@
 #include <BleGamepad.h>
-
-//If using more than 4 buttons, declare them here and change below NUM_BUTTONS
 #define BUTTON_PIN1 12  // GPIO12
 #define BUTTON_PIN2 14  // GPIO14
 #define BUTTON_PIN3 26  // GPIO26
 #define BUTTON_PIN4 27  // GPIO27
-
-//Number of buttons connected. You can connect more and change it here
 #define NUM_BUTTONS 4
+BleGamepad bleGamepad ("Yuko's Gamepad", "Yuko", 100);
 
-BleGamepad bleGamepad ("Tech Talkies Gamepad", "Tech Talkies", 100);
-
-//JOYSTICKS (Swap pins if directions are inverted)
+//joysticks; switch the pin values if you change INVERT_Y/INVERT_X bools.
 #define VRX_JOYSTICK 15
 #define VRY_JOYSTICK 4
 
-//If only Y axis is inverted, switch this true/false
 #define INVERT_Y true
 #define INVERT_X true
 
 int buttonPins[NUM_BUTTONS] = { BUTTON_PIN1, BUTTON_PIN2, BUTTON_PIN3, BUTTON_PIN4 };
-
-/*Default library buttons. If you have specific button values, add them here
-On Android, mapping is as follows
-BUTTON_1 - A
-BUTTON_2 - B
-BUTTON_4 - X
-BUTTON_5 - Y
-*/
 int buttons[NUM_BUTTONS] = { BUTTON_1, BUTTON_2, BUTTON_4, BUTTON_5 };
 
-uint16_t VrxReading = 0;
-uint16_t VryReading = 0;
-uint16_t VrxValue = 0;
-uint16_t VryValue = 0;
+int VrxReading = 0;
+int VryReading = 0;
+int VrxValue = 0;
+int VryValue = 0;
 
 void setup() {
   Serial.begin(115200);
-  // initialize the push-button pins as an pull-up input
+  // initialize the push-button pins as pull-up input
   pinMode(BUTTON_PIN1, INPUT_PULLUP);
   pinMode(BUTTON_PIN2, INPUT_PULLUP);
   pinMode(BUTTON_PIN3, INPUT_PULLUP);
   pinMode(BUTTON_PIN4, INPUT_PULLUP);
-
   bleGamepad.begin();
 }
 
@@ -55,26 +40,27 @@ void loop() {
     VrxValue = map(VrxReading, 4095, 0, 0, 32737);
     VryValue = map(VryReading, 4095, 0, 0, 32737);
 
-    uint16_t VrxCenter = 32737 / 2;
-    uint16_t VryCenter = 32737 / 2;
+    int VrxCenter = 32737 / 2;
+    int VryCenter = 32737 / 2;
 
     if (VrxValue < VrxCenter + 2000 && VrxValue > VrxCenter - 2000) {
       VrxValue = VrxCenter;
     }
-
-    if (VryValue < VryCenter + 2000 && VryValue > VryCenter - 2000) {
+      if (VryValue < VryCenter + 2000 && VryValue > VryCenter - 2000) {
       VryValue = VryCenter;
     }
 
     if (INVERT_Y) {
       VryValue = 32737 - VryValue;
-      if (VryValue < 0)
+      if (VryValue < 0) {
         VryValue = -VryValue;
+      }
     }
     if (INVERT_X) {
       VrxValue = 32737 - VrxValue;
-      if (VrxValue < 0)
+      if (VrxValue < 0) {
         VrxValue = -VrxValue;
+      }
     }
 
     bleGamepad.setLeftThumb(VrxValue, VryValue);
@@ -82,7 +68,8 @@ void loop() {
     for (int i = 0; i < NUM_BUTTONS; i++) {
       if (!digitalRead(buttonPins[i])) {
         bleGamepad.press(buttons[i]);
-      } else {
+        }
+      else {
         bleGamepad.release(buttons[i]);
       }
     }
